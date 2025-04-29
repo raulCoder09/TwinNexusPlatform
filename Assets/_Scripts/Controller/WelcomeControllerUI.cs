aa8f3dbcb4cc1b7e76da153cb90ce91002c63f5e
Adán Raúl Morales Hernández <98626075+raulCoder09@users.noreply.github.com>
2025-04-29 16:26:08 -0600

    Se concluye la animacion del submenu de registro de nuevo usuario

--- Assets/_Scripts/Controller/WelcomeControllerUI.cs
+++ Assets/_Scripts/Controller/WelcomeControllerUI.cs
@@ -8,6 +8,7 @@ namespace _Scripts.Controller
         private VisualElement _subpanelsAndSmokeMaskContainer;
         private Button _launchButton;
         private Button _closeLoginPanelButton;
+        private Button _closeRegisterPanelButton;
         private Button _exitAppButton;
         private VisualElement _loginPanel;
         private VisualElement _scrim;
@@ -17,13 +18,15 @@ namespace _Scripts.Controller
         private Button _recoverPasswordLoginButton;
         private Button _loginButton;
 
+        private Button _backToLoginPanelButton;
+
         
         private void Start()
         {
             var root = GetComponent<UIDocument>().rootVisualElement;
             _subpanelsAndSmokeMaskContainer = root.Q<VisualElement>("SubpanelsAndSmokeMaskContainer");
             _launchButton = root.Q<Button>("LaunchButton");
-            _closeLoginPanelButton = root.Q<Button>("CloseButton");
+            _closeLoginPanelButton = root.Q<Button>("CloseLoginButton");
             _exitAppButton=root.Q<Button>("ExitButton");
             _loginPanel = root.Q<VisualElement>("LoginPanel");
             _registerPanel= root.Q<VisualElement>("RegisterPanel");
@@ -31,7 +34,8 @@ namespace _Scripts.Controller
             _registerLoginButton=root.Q<Button>("RegisterLoginButton");
             _recoverPasswordLoginButton=root.Q<Button>("RecoverPasswordLoginButton");
             _loginButton=root.Q<Button>("LoginButton");
-            
+            _backToLoginPanelButton=root.Q<Button>("BackToLoginPanelButton");
+            _closeRegisterPanelButton=root.Q<Button>("CloseRegisterPanelButton");
 
             _subpanelsAndSmokeMaskContainer.style.display = DisplayStyle.None;
 
@@ -41,8 +45,27 @@ namespace _Scripts.Controller
             _registerLoginButton.RegisterCallback<ClickEvent>(CloseLoginPanelAndOpenRegisterPanel);
             _recoverPasswordLoginButton.RegisterCallback<ClickEvent>(CloseLoginPanel);
             _loginButton.RegisterCallback<ClickEvent>(CloseLoginPanel);
+            _closeRegisterPanelButton.RegisterCallback<ClickEvent>(CloseRegisterPanel);
                 
+            
+            _backToLoginPanelButton.RegisterCallback<ClickEvent>(CloseRegisterPanelAndOpenLoginPanel);
             _loginPanel.RegisterCallback<TransitionEndEvent>(OnLoginPanelTransitionComplete);
+            _registerPanel.RegisterCallback<TransitionEndEvent>(OnLoginRegisterTransitionComplete);
+        }
+
+        private void OnLoginRegisterTransitionComplete(TransitionEndEvent evt)
+        {
+            if (!_loginPanel.ClassListContains("LoginPanelMoveB") && !_registerPanel.ClassListContains("RegisterPanelInMainScreen"))
+            {
+                _subpanelsAndSmokeMaskContainer.style.display = DisplayStyle.None;
+            }
+        }
+
+
+        private void CloseRegisterPanelAndOpenLoginPanel(ClickEvent evt)
+        {
+            CloseRegisterPanel(evt);
+            OpenLoginPanel(evt);
         }
 
         private void CloseLoginPanelAndOpenRegisterPanel(ClickEvent evt)
@@ -81,7 +104,16 @@ namespace _Scripts.Controller
 
         private void OpenRegisterPanel()
         {
+            _subpanelsAndSmokeMaskContainer.style.display = DisplayStyle.Flex;
             _registerPanel.AddToClassList("RegisterPanelInMainScreen");
+            _scrim.AddToClassList("ScrimFadein");
+        }
+
+        private void CloseRegisterPanel(ClickEvent evt)
+        {
+            _registerPanel.RemoveFromClassList("RegisterPanelInMainScreen");
+            _registerPanel.AddToClassList("RegisterPanelOutMainScreen"); 
+            _scrim.RemoveFromClassList("ScrimFadein");
         }
         
     }
