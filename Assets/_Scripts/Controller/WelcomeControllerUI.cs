2d20d831aee176701704dfdc04242eb83af7c585
Adán Raúl Morales Hernández <98626075+raulCoder09@users.noreply.github.com>
2025-04-29 14:14:07 -0600

    update

--- Assets/_Scripts/Controller/WelcomeControllerUI.cs
+++ Assets/_Scripts/Controller/WelcomeControllerUI.cs
@@ -5,29 +5,52 @@ namespace _Scripts.Controller
 {
     public class WelcomeControllerUI : MonoBehaviour
     {
-        private VisualElement _bottomContainer;
+        private VisualElement _subpanelsAndSmokeMaskContainer;
         private Button _launchButton;
-        private Button _closeButton;
-        private Button _exitButton;
+        private Button _closeLoginPanelButton;
+        private Button _exitAppButton;
         private VisualElement _loginPanel;
         private VisualElement _scrim;
 
-        void Start()
+        private Button _registerLoginButton;
+        private Button _recoverPasswordLoginButton;
+        private Button _loginButton;
+        
+        // RegisterLoginButton
+        //     RecoverPasswordLoginButton
+        private void Start()
         {
             var root = GetComponent<UIDocument>().rootVisualElement;
-            _bottomContainer = root.Q<VisualElement>("BottomContainer");
+            _subpanelsAndSmokeMaskContainer = root.Q<VisualElement>("SubpanelsAndSmokeMaskContainer");
             _launchButton = root.Q<Button>("LaunchButton");
-            _closeButton = root.Q<Button>("CloseButton");
-            _exitButton=root.Q<Button>("ExitButton");
+            _closeLoginPanelButton = root.Q<Button>("CloseButton");
+            _exitAppButton=root.Q<Button>("ExitButton");
             _loginPanel = root.Q<VisualElement>("LoginPanel");
             _scrim = root.Q<VisualElement>("Scrim");
+            _registerLoginButton=root.Q<Button>("RegisterLoginButton");
+            _recoverPasswordLoginButton=root.Q<Button>("RecoverPasswordLoginButton");
+            _loginButton=root.Q<Button>("LoginButton");
+            
 
-            _bottomContainer.style.display = DisplayStyle.None;
+            _subpanelsAndSmokeMaskContainer.style.display = DisplayStyle.None;
 
-            _launchButton.RegisterCallback<ClickEvent>(OpenSubMenu);
-            _closeButton.RegisterCallback<ClickEvent>(CloseSubMenu);
-            _exitButton.RegisterCallback<ClickEvent>(ExitAplication);
-            _loginPanel.RegisterCallback<TransitionEndEvent>(OnBottomLoginDown);
+            _launchButton.RegisterCallback<ClickEvent>(OpenLoginPanel);
+            _closeLoginPanelButton.RegisterCallback<ClickEvent>(CloseLoginPanel);
+            _exitAppButton.RegisterCallback<ClickEvent>(ExitAplication);
+            _registerLoginButton.RegisterCallback<ClickEvent>(CloseLoginPanelAndOpenRegisterPanel);
+            _recoverPasswordLoginButton.RegisterCallback<ClickEvent>(CloseLoginPanel);
+            _loginButton.RegisterCallback<ClickEvent>(CloseLoginPanel);
+                
+            _loginPanel.RegisterCallback<TransitionEndEvent>(OnLoginPanelTransitionComplete);
+        }
+
+        private void CloseLoginPanelAndOpenRegisterPanel(ClickEvent evt)
+        {
+            CloseLoginPanel(evt);
+            // si se detecto que la animacion del loginpanel ya termino
+            //     se debe abrir el panel register es decir si se dio click en el boton register y la animacion de closeloginpanel termino 
+            //         el metodo OpenRegisterPanel(evt); debe ejecutarse
+
         }
 
         private void ExitAplication(ClickEvent evt)
@@ -35,25 +58,26 @@ namespace _Scripts.Controller
             Application.Quit();
         }
 
-        private void OpenSubMenu(ClickEvent evt)
+        private void OpenLoginPanel(ClickEvent evt)
         {
-            _bottomContainer.style.display = DisplayStyle.Flex;
+            _subpanelsAndSmokeMaskContainer.style.display = DisplayStyle.Flex;
             _loginPanel.AddToClassList("LoginPanelMoveB");
             _scrim.AddToClassList("ScrimFadein");
         }
 
-        private void CloseSubMenu(ClickEvent evt)
+        private void CloseLoginPanel(ClickEvent evt)
         {
             _loginPanel.RemoveFromClassList("LoginPanelMoveB");
             _loginPanel.AddToClassList("LoginPanelMoveA"); 
             _scrim.RemoveFromClassList("ScrimFadein");
         }
 
-        private void OnBottomLoginDown(TransitionEndEvent evt)
+        private void OnLoginPanelTransitionComplete(TransitionEndEvent evt)  //revisar es el nombe mas adecuado
         {
+
             if (!_loginPanel.ClassListContains("LoginPanelMoveB"))
             {
-                _bottomContainer.style.display = DisplayStyle.None;
+                _subpanelsAndSmokeMaskContainer.style.display = DisplayStyle.None;
             }
         }
 
