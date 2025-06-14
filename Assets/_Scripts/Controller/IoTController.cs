ed9bb8dad2208631c9a8d928bc627409b0c6dca9
Adan Raul Morales Hernandez <mechar09@outlook.com>
2025-06-14 11:38:44 -0600

    se logra comunicacion con raspberry y unity

--- Assets/_Scripts/Controller/IoTController.cs
+++ Assets/_Scripts/Controller/IoTController.cs
@@ -6,108 +6,108 @@ namespace _Scripts.Controller
 {
     public class IoTController : MonoBehaviour
     {
-        private VisualElement _body;
-        private TextField _ipOrHostnameTextField;
-        private TextField _portTextField;
-        private TextField _clientIDTextField;
-        private DropdownField _connectionTypeDropdownField;
-        private DropdownField _modeConnectionDropdownField;
-        private DropdownField _protocolCommunicationDropdownField;
-        private TextField _usernameTextField;
-        private TextField _passwordTextField;
-        private Button _testLocalButton;
-        private Button _connectLocalButton;
-        private Button _disconnectLocalButton;
-        private Label _statusLocalLabel;
-        
-        
-        private void Awake()
-        {
-            GetUiComponents();
-            RegisterEvents();
-            FindObjects();
-        }
-        
-        private void Start()
-        {
-            HideUi();
-            _usernameTextField.style.display = DisplayStyle.None;
-            _passwordTextField.style.display = DisplayStyle.None;
-            _connectionTypeDropdownField.value = "Connection type";
-            _modeConnectionDropdownField.value = "Mode connection";
-            _protocolCommunicationDropdownField.value = "Protocol communication";
-        }
-        
-
-        internal void HideUi()
-        {
-            _body.style.display = DisplayStyle.None;
-        }
-        internal void ShowUi()
-        {
-            _body.style.display = DisplayStyle.Flex;
-        }
-
-        private void FindObjects()
-        {
-        }
-
-        private void RegisterEvents()
-        {
-            _ipOrHostnameTextField.RegisterValueChangedCallback(evt =>
-            {
-                
-            });
-            
-            _portTextField.RegisterValueChangedCallback(evt =>
-            {
-                
-            });
-            
-            _clientIDTextField.RegisterValueChangedCallback(evt =>
-            {
-                
-            });
-            _connectionTypeDropdownField.RegisterValueChangedCallback(evt =>
-            {
-                
-            });
-            _modeConnectionDropdownField.RegisterValueChangedCallback(evt =>
-            {
-                
-            });
-            
-            _protocolCommunicationDropdownField.RegisterValueChangedCallback(evt =>
-            {
-                print(evt.newValue);
-            });
-            _usernameTextField.RegisterValueChangedCallback(evt =>
-            {
-                
-            });
-            _passwordTextField.RegisterValueChangedCallback(evt =>
-            {
-                
-            });
-                
-        }
-
-        private void GetUiComponents()
-        {
-            var root = GetComponent<UIDocument>().rootVisualElement;
-            _body = root.Q<VisualElement>("Body");
-            _ipOrHostnameTextField= root.Q<TextField>("IPOrHostnameTextField");
-            _portTextField= root.Q<TextField>("PortTextField");
-            _clientIDTextField= root.Q<TextField>("ClientIDTextField");
-            _connectionTypeDropdownField=root.Q<DropdownField>("ConnectionTypeDropdownField");
-            _modeConnectionDropdownField=root.Q<DropdownField>("ModeConnectionDropdownField");
-            _protocolCommunicationDropdownField=root.Q<DropdownField>("ProtocolCommunicationDropdownField");
-            _usernameTextField=root.Q<TextField>("UsernameTextField");
-            _passwordTextField=root.Q<TextField>("PasswordTextField");
-            _testLocalButton=root.Q<Button>("TestLocalButton");
-            _connectLocalButton=root.Q<Button>("ConnectLocalButton");
-            _disconnectLocalButton=root.Q<Button>("DisconnectLocalButton");
-            _statusLocalLabel=root.Q<Label>("StatusLocalLabel");
-        }
+        // private VisualElement _body;
+        // private TextField _ipOrHostnameTextField;
+        // private TextField _portTextField;
+        // private TextField _clientIDTextField;
+        // private DropdownField _connectionTypeDropdownField;
+        // private DropdownField _modeConnectionDropdownField;
+        // private DropdownField _protocolCommunicationDropdownField;
+        // private TextField _usernameTextField;
+        // private TextField _passwordTextField;
+        // private Button _testLocalButton;
+        // private Button _connectLocalButton;
+        // private Button _disconnectLocalButton;
+        // private Label _statusLocalLabel;
+        //
+        //
+        // private void Awake()
+        // {
+        //     GetUiComponents();
+        //     RegisterEvents();
+        //     FindObjects();
+        // }
+        //
+        // private void Start()
+        // {
+        //     HideUi();
+        //     _usernameTextField.style.display = DisplayStyle.None;
+        //     _passwordTextField.style.display = DisplayStyle.None;
+        //     _connectionTypeDropdownField.value = "Connection type";
+        //     _modeConnectionDropdownField.value = "Mode connection";
+        //     _protocolCommunicationDropdownField.value = "Protocol communication";
+        // }
+        //
+        //
+        // internal void HideUi()
+        // {
+        //     _body.style.display = DisplayStyle.None;
+        // }
+        // internal void ShowUi()
+        // {
+        //     _body.style.display = DisplayStyle.Flex;
+        // }
+        //
+        // private void FindObjects()
+        // {
+        // }
+        //
+        // private void RegisterEvents()
+        // {
+        //     _ipOrHostnameTextField.RegisterValueChangedCallback(evt =>
+        //     {
+        //         
+        //     });
+        //     
+        //     _portTextField.RegisterValueChangedCallback(evt =>
+        //     {
+        //         
+        //     });
+        //     
+        //     _clientIDTextField.RegisterValueChangedCallback(evt =>
+        //     {
+        //         
+        //     });
+        //     _connectionTypeDropdownField.RegisterValueChangedCallback(evt =>
+        //     {
+        //         
+        //     });
+        //     _modeConnectionDropdownField.RegisterValueChangedCallback(evt =>
+        //     {
+        //         
+        //     });
+        //     
+        //     _protocolCommunicationDropdownField.RegisterValueChangedCallback(evt =>
+        //     {
+        //         print(evt.newValue);
+        //     });
+        //     _usernameTextField.RegisterValueChangedCallback(evt =>
+        //     {
+        //         
+        //     });
+        //     _passwordTextField.RegisterValueChangedCallback(evt =>
+        //     {
+        //         
+        //     });
+        //         
+        // }
+        //
+        // private void GetUiComponents()
+        // {
+        //     var root = GetComponent<UIDocument>().rootVisualElement;
+        //     _body = root.Q<VisualElement>("Body");
+        //     _ipOrHostnameTextField= root.Q<TextField>("IPOrHostnameTextField");
+        //     _portTextField= root.Q<TextField>("PortTextField");
+        //     _clientIDTextField= root.Q<TextField>("ClientIDTextField");
+        //     _connectionTypeDropdownField=root.Q<DropdownField>("ConnectionTypeDropdownField");
+        //     _modeConnectionDropdownField=root.Q<DropdownField>("ModeConnectionDropdownField");
+        //     _protocolCommunicationDropdownField=root.Q<DropdownField>("ProtocolCommunicationDropdownField");
+        //     _usernameTextField=root.Q<TextField>("UsernameTextField");
+        //     _passwordTextField=root.Q<TextField>("PasswordTextField");
+        //     _testLocalButton=root.Q<Button>("TestLocalButton");
+        //     _connectLocalButton=root.Q<Button>("ConnectLocalButton");
+        //     _disconnectLocalButton=root.Q<Button>("DisconnectLocalButton");
+        //     _statusLocalLabel=root.Q<Label>("StatusLocalLabel");
+        // }
     }
 }
--- Assets/_Scripts/Models/MQTTProtocol.cs
+++ Assets/_Scripts/Models/MQTTProtocol.cs
@@ -1,22 +1,89 @@
-using MQTTnet.Client;
+using System;
 using UnityEngine;
+using MQTTnet;
+using MQTTnet.Client;
+using System.Threading.Tasks;
 
 namespace _Scripts.Models
 {
-
     public class MQTTProtocol : MonoBehaviour
     {
-        private IMqttClient _mqttClient;
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
+        // Fábrica para crear el cliente MQTT
+        private MqttFactory _factory;
         
+        private IMqttClient _client;
+        
+        private string _brokerAddress = "192.168.8.94"; // IP del broker
+        
+        private int _brokerPort = 1883; // Puerto estándar de MQTT
+        
+        private string _username = "ruloCoder09"; // Usuario configurado en Mosquitto
+        
+        private string _password = "5859"; // Contraseña del usuario
+        
+        private string _clientId = "UnityClient"; // ID del cliente en Unity
+        
+        private string _subscribeTopic = "test/topic"; // Tema para suscribirse
+        
+        private async Task ConnectToBroker()
+        {
+            _factory = new MqttFactory();
+            _client = _factory.CreateMqttClient();
+            
+            var options = new MqttClientOptionsBuilder()
+                .WithClientId(_clientId)
+                .WithTcpServer(_brokerAddress, _brokerPort)
+                .WithCredentials(_username, _password)
+                .Build();
+            
+            _client.ConnectedAsync += async e =>
+            {
+                Debug.Log("Conectado al broker MQTT en " + _brokerAddress);
+                await _client.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic(_subscribeTopic).Build());
+                Debug.Log("Suscrito al tema: " + _subscribeTopic);
+            };
+            
+            _client.DisconnectedAsync += async e =>
+            {
+                Debug.Log("Desconectado del broker MQTT");
+            };
+
+            _client.ApplicationMessageReceivedAsync += e =>
+            {
+                string message = System.Text.Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                Debug.Log("Mensaje recibido en " + e.ApplicationMessage.Topic + ": " + message);
+                return Task.CompletedTask;
+            };
+
+            try
+            {
+                await _client.ConnectAsync(options);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("Error al conectar al broker: " + ex.Message);
+            }
+        }
+        
+        async void Start()
+        {
+            try
+            {
+                await ConnectToBroker();
+            }
+            catch (Exception e)
+            {
+                throw; // TODO handle exception
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnDestroy()
         {
-        
+            if (_client != null && _client.IsConnected)
+            {
+                _client.DisconnectAsync().Wait();
+                Debug.Log("Cliente MQTT desconectado");
+            }
         }
     }
 }
