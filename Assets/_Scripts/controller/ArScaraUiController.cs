ad69ebd9ba3cbb6bf287a36cc391b42eece6610a
Adán Raúl Morales Hernández <98626075+raulCoder09@users.noreply.github.com>
2025-09-04 17:50:24 -0600

    navegacion completa ahora debo ajustar la ui de jog and teach

--- Assets/_Scripts/controller/ArScaraUiController.cs
+++ Assets/_Scripts/controller/ArScaraUiController.cs
@@ -20,17 +20,121 @@ public class ArScaraUiController : MonoBehaviour
                 _root.Q<VisualElement>("slidingPanels").style.display = DisplayStyle.None;
         });
         
+        _root.Q<DropdownField>("enviromentMenu")?.RegisterValueChangedCallback(evt =>
+        {
+            
+            EnviromentMenu(evt.newValue);
+        });
+        
+        _root.Q<DropdownField>("arscaraMenu")?.RegisterValueChangedCallback(evt =>
+        {
+
+            ArscaraMenu(evt.newValue);
+        });
+        
     }
+
+    private void EnviromentMenu(string enviroment)
+    {
+        switch (enviroment)
+        {
+            case "Virtual":
+                _root.Q<DropdownField>("arscaraMenu").SetEnabled(true);
+                _root.Q<Label>("warningMessages").text = "Select ARSCARA robot interface";
+                break;
+            case "Augmented":
+                _root.Q<DropdownField>("arscaraMenu").SetEnabled(true);
+                _root.Q<Label>("warningMessages").text = "Select ARSCARA robot interface";
+                break;
+            case "Hybrid":
+                _root.Q<DropdownField>("arscaraMenu").SetEnabled(true);
+                _root.Q<Label>("warningMessages").text = "Select ARSCARA robot interface";
+                break;
+            case "Real":
+                _root.Q<DropdownField>("arscaraMenu").SetEnabled(true);
+                _root.Q<Label>("warningMessages").text = "Select ARSCARA robot interface";
+                break;
+            default:
+                _root.Q<DropdownField>("arscaraMenu").SetEnabled(false);
+                _root.Q<Label>("warningMessages").text = "Select a work environment";
+                break;
+        }
+    }
+
+    private void ArscaraMenu(string value)
+    {
+        switch (value)
+        {
+            case "Control panel":
+                _root.Q<Label>("warningMessages").style.display = DisplayStyle.None;
+                _root.Q<VisualElement>("controlPanel").RemoveFromClassList("arScaraPanelsOut");
+                _root.Q<VisualElement>("controlPanel").AddToClassList("arScaraPanelsIn");
+                _root.Q<VisualElement>("jogAndTeachPanel").RemoveFromClassList("arScaraPanelsIn");
+                _root.Q<VisualElement>("jogAndTeachPanel").AddToClassList("arScaraPanelsOut");
+                _root.Q<VisualElement>("pointsPanel").RemoveFromClassList("arScaraPanelsIn");
+                _root.Q<VisualElement>("pointsPanel").AddToClassList("arScaraPanelsOut");
+                break;
+            case "Jog and teach":
+                _root.Q<Label>("warningMessages").style.display = DisplayStyle.None;
+                _root.Q<VisualElement>("controlPanel").RemoveFromClassList("arScaraPanelsIn");
+                _root.Q<VisualElement>("controlPanel").AddToClassList("arScaraPanelsOut");
+                _root.Q<VisualElement>("jogAndTeachPanel").RemoveFromClassList("arScaraPanelsOut");
+                _root.Q<VisualElement>("jogAndTeachPanel").AddToClassList("arScaraPanelsIn");
+                _root.Q<VisualElement>("pointsPanel").RemoveFromClassList("arScaraPanelsIn");
+                _root.Q<VisualElement>("pointsPanel").AddToClassList("arScaraPanelsOut");
+                break;
+            case "Points":
+                _root.Q<Label>("warningMessages").style.display = DisplayStyle.None;
+                _root.Q<VisualElement>("controlPanel").RemoveFromClassList("arScaraPanelsIn");
+                _root.Q<VisualElement>("controlPanel").AddToClassList("arScaraPanelsOut");
+                _root.Q<VisualElement>("jogAndTeachPanel").RemoveFromClassList("arScaraPanelsIn");
+                _root.Q<VisualElement>("jogAndTeachPanel").AddToClassList("arScaraPanelsOut");
+                _root.Q<VisualElement>("pointsPanel").RemoveFromClassList("arScaraPanelsOut");
+                _root.Q<VisualElement>("pointsPanel").AddToClassList("arScaraPanelsIn");
+                break;
+            default:
+                _root.Q<Label>("warningMessages").style.display = DisplayStyle.Flex;
+                if (value=="Enviroment")
+                {
+                    _root.Q<Label>("warningMessages").text = "Select a work environment";
+                }
+                else
+                {
+                    _root.Q<Label>("warningMessages").text = "Select ARSCARA robot interface";
+                }
+                
+                _root.Q<VisualElement>("controlPanel").RemoveFromClassList("arScaraPanelsIn");
+                _root.Q<VisualElement>("controlPanel").AddToClassList("arScaraPanelsOut");
+                _root.Q<VisualElement>("jogAndTeachPanel").RemoveFromClassList("arScaraPanelsIn");
+                _root.Q<VisualElement>("jogAndTeachPanel").AddToClassList("arScaraPanelsOut");
+                _root.Q<VisualElement>("pointsPanel").RemoveFromClassList("arScaraPanelsIn");
+                _root.Q<VisualElement>("pointsPanel").AddToClassList("arScaraPanelsOut");
+                break;
+        }
+    }
+
     private void Start()
     {
         _root.Q<VisualElement>("slidingPanels").style.display = DisplayStyle.None;
         _root.Q<DropdownField>("enviromentMenu").value = "Enviroment";
         _root.Q<DropdownField>("arscaraMenu").value = "ARSCARA menu";
+        _root.Q<DropdownField>("views").value = "Select view";
+        _root.Q<DropdownField>("modeDropdown").value = "Mode";
+        _root.Q<Label>("warningMessages").text = "Select a work environment";
     }
 
     private void ShowMainMenu(ClickEvent evt)
     {
         ShowPanel("navigationMenuPanel","mainMenuPanel");
+        _root.Q<DropdownField>("enviromentMenu").value = "Enviroment";
+        _root.Q<DropdownField>("arscaraMenu").value = "ARSCARA menu";
+        _root.Q<Label>("warningMessages").text = "Select a work environment";
+        _root.Q<VisualElement>("controlPanel").RemoveFromClassList("arScaraPanelsIn");
+        _root.Q<VisualElement>("controlPanel").AddToClassList("arScaraPanelsOut");
+        _root.Q<VisualElement>("jogAndTeachPanel").RemoveFromClassList("arScaraPanelsIn");
+        _root.Q<VisualElement>("jogAndTeachPanel").AddToClassList("arScaraPanelsOut");
+        _root.Q<VisualElement>("pointsPanel").RemoveFromClassList("arScaraPanelsIn");
+        _root.Q<VisualElement>("pointsPanel").AddToClassList("arScaraPanelsOut");
     }
 
     private void HideMainMenu(ClickEvent evt)
