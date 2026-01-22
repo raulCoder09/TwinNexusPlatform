6e1696074cce5ef80ac087b4dc9b0f2cdbc27b4e
armh <mechar09@gmail.com>
2026-01-22 03:43:51 -0600

    se logra compilacion y se corrige problema de ids

--- Assets/_scripts/controllers/ArScaraEnvironmentController.cs
+++ Assets/_scripts/controllers/ArScaraEnvironmentController.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.XR.Management;
+
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
